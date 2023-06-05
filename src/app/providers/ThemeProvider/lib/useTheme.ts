bd40a1435361a5e18285257b80217b3ee7ab675f
Coreytaz <coreytaz321@gmail.com>
2023-06-05 18:09:51 +0500

    add theme color system

--- src/app/providers/ThemeProvider/lib/useTheme.ts
+++ src/app/providers/ThemeProvider/lib/useTheme.ts
@@ -1,23 +1,35 @@
-import { useContext } from 'react';
+import { useContext, useEffect } from 'react';
 
 import { LOCAL_STORAGE_THEME_KEY, Theme, ThemeContext } from './ThemeContext';
 
 interface UseThemeResult {
-    toggleTheme: () => void;
-    theme: Theme;
+  toggleTheme: () => void;
+  theme: Theme;
 }
 
 export function useTheme(): UseThemeResult {
-    const { theme, setTheme } = useContext(ThemeContext);
+  const { theme, setTheme } = useContext(ThemeContext);
 
-    const toggleTheme = () => {
-        const newTheme = theme === Theme.DARK ? Theme.LIGHT : Theme.DARK;
-        setTheme!(newTheme);
-        localStorage.setItem(LOCAL_STORAGE_THEME_KEY, newTheme);
-    };
+  useEffect(() => {
+    const newTheme =
+      localStorage.getItem(LOCAL_STORAGE_THEME_KEY) !== null
+        ? (localStorage.getItem(LOCAL_STORAGE_THEME_KEY) as Theme)
+        : window.matchMedia?.('(prefers-color-scheme: dark)').matches
+        ? Theme.DARK
+        : Theme.LIGHT;
 
-    return {
-        theme: theme!,
-        toggleTheme,
-    };
+    setTheme!(newTheme);
+    localStorage.setItem(LOCAL_STORAGE_THEME_KEY, newTheme);
+  }, [setTheme]);
+
+  const toggleTheme = () => {
+    const newTheme = theme === Theme.DARK ? Theme.LIGHT : Theme.DARK;
+    setTheme!(newTheme);
+    localStorage.setItem(LOCAL_STORAGE_THEME_KEY, newTheme);
+  };
+
+  return {
+    theme: theme!,
+    toggleTheme,
+  };
 }
