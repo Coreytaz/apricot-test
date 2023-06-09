0708b78af58ff80bf311dea893a8d29c80c0fc0f
Coreytaz <coreytaz321@gmail.com>
2023-06-09 18:17:58 +0500

    add entities CardTodo

---
+++ src/entities/CardTodo/ui/index.tsx
@@ -0,0 +1,15 @@
+import { FC } from 'react'
+
+import { Todos } from '~entities/todo'
+
+import cls from './styles.module.scss'
+
+export const CardTodo: FC<Todos> = ({ id, text, description }) => {
+    return (
+        <div key={id} className={cls.card}>
+            <div><span>ID:</span><p>{id}</p></div>
+            <div style={{ textAlign: 'center' }}>Название задания <p>{text}</p></div>
+            <div style={{ textAlign: 'center' }}>Описания задания <p>{description?.length! > 0 ? description : 'Отсутствует'}</p></div>
+        </div>
+    )
+}
