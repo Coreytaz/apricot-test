5abbfcc53167b50824708d9787deac33a156cbea
Coreytaz <coreytaz321@gmail.com>
2023-06-09 18:19:27 +0500

    add Icon

---
+++ src/shared/assets/index.ts
@@ -0,0 +1,10 @@
+import { ReactComponent as Chevron } from './icons/chevron-left.svg';
+import { ReactComponent as X } from './icons/X.svg';
+import { ReactComponent as ChevronDown } from './icons/ChevronDown.svg';
+import { ReactComponent as Check } from './icons/check.svg';
+import { ReactComponent as Son } from './icons/theme-light.svg';
+import { ReactComponent as Moon } from './icons/theme-dark.svg';
+import { ReactComponent as Trash } from './icons/trash.svg';
+import { ReactComponent as PlusSquare } from './icons/plus-square.svg';
+
+export { Chevron, X, ChevronDown, Check, Moon, Son, Trash, PlusSquare };
